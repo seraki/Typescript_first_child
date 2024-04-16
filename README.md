246cbbc9ff0c1096bf8675a64119b8e89b374ab9
seraki <47520575+seraki@users.noreply.github.com>
2024-04-16 15:36:58 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Typescript_first_child
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/seraki/Typescript_first_child)
---
+++ index.ts
@@ -0,0 +1,9 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1> Mimi is here to stay </h1>`;
+
+const buttonOn: HTMLElement = document.getElementById('app');
+
